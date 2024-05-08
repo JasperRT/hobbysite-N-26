9b48793301b3e29dd235c1f34498a2ea3f16f678
Jasper <jasper.sevillejo@student.ateneo.edu>
2024-05-08 16:38:36 +0800

    Added user_management to the project's apps.

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'merchstore',
-    'commissions'
+    'commissions',
+    'user_management'
 ]
 
 MIDDLEWARE = [
