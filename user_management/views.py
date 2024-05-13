4655f13b8d318147c76aeae3e0a79b6f6f178c44
Jasper <jasper.sevillejo@student.ateneo.edu>
2024-05-13 10:37:00 +0800

    Added the user_management app.

--- user_management/views.py
+++ user_management/views.py
@@ -1,18 +1,20 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.urls import reverse
+
 from .models import Profile
+from .forms import ProfileForm
 
 @login_required
 def update_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    update_form = ProfileForm()
+    ctx = { "profile": profile, "update_form": update_form }
     if request.method == 'POST':
-        user = User.objects.get(username=request.user.username)
-        user.email = request.POST['email']
-        user.save()
-        profile = Profile.objects.get(user=user)
-        profile.display_name = request.POST['display_name']
-        profile.save()
-        return redirect('profile')
-
-    return render(request, 'user_management/profile_update.html')
+        update_form = ProfileForm(request.POST)
+        if update_form.is_valid():
+            profile.display_name = update_form.instance.display_name
+            profile.email = update_form.instance.email
+            profile.save()
+            return render(request, 'user_management/profile_update.html', ctx)
+    else:
+        return render(request, 'user_management/profile_update.html', ctx)
